7e8c256c3c8e92e3e080e5cfbbcbe9bbaec4b03d
junaidmallick <junaidmallick1805@gmail.com>
2022-03-05 00:35:49 +0530

    middlewareintro

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  const dateTime = new Date().toISOString().replace(/\..+/, "");
+  const IP = req.ip;
+  const api = req.method + req.originalUrl;
+  console.log("Timestamp:", dateTime);
+  console.log("-----------------------------------");
+  console.log("IP address ", IP);
+  console.log("-----------------------------------");
+  console.log("Fired API", api);
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+  next();
+});
 
-app.use('/', route);
+mongoose
+  .connect(
+    "mongodb+srv://thorium-cohort:qwertyuiop@cluster0.xyklh.mongodb.net/junaid?authSource=admin&replicaSet=atlas-wc30tm-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
+app.use("/", route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
