b48ee93150dc9dc09ae37fd41ccc6554f543bc87
junaidmallick <junaidmallick1805@gmail.com>
2022-03-07 10:24:05 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
+
 const route = require("./routes/route.js");
+const moment = require("moment");
+
 const { default: mongoose } = require("mongoose");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-  const dateTime = new Date().toISOString().replace(/\..+/, "");
+  let now = new Date();
+
+  const dateTime = new moment(now).format("YYYY-MM-DD HH:mm:ss:a");
+
   const IP = req.ip;
-  const api = req.method + req.originalUrl;
-  console.log("Timestamp:", dateTime);
-  console.log("-----------------------------------");
-  console.log("IP address ", IP);
-  console.log("-----------------------------------");
-  console.log("Fired API", api);
+
+  const Api = req.method + req.originalUrl;
+
+  console.log("Timestamp:", dateTime, " ", " IP address ", IP, " ", "API", Api);
 
   next();
 });
